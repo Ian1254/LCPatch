8c4883344624fb7e03eea14c6fec455cc41cf849
FallenSakuraSky <chenian1254chenian@gmail.com>
2026-09-14 23:59:45 +0800

    更新 build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lcpatch"
         minSdk = 28
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0-beta"
+        versionCode = 3
+        versionName = "1.1.1-beta"
         ndk { abiFilters += "arm64-v8a" }
     }
 
