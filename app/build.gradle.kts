5a1289f8f4285403cc0b9421c22c879e4622ff2b
FallenSakuraSky <chenian1254chenian@gmail.com>
2026-09-15 02:58:22 +0800

    chore: prepare 1.2.0-beta.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lcpatch"
         minSdk = 28
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.1.1-beta"
+        versionCode = 4
+        versionName = "1.2.0-beta.1"
         ndk { abiFilters += "arm64-v8a" }
     }
 
