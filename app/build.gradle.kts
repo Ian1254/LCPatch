2f788d60a092376f93389945bbb8debf0c9178ec
FallenSakuraSky <chenian1254chenian@gmail.com>
2026-09-15 23:27:12 +0800

    chore: bump version to 1.2.0-beta.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.lcpatch"
         minSdk = 28
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.2.0-beta.7"
+        versionCode = 11
+        versionName = "1.2.0-beta.8"
         ndk { abiFilters += "arm64-v8a" }
     }
 
